1aa24ae9ab3a302e123738b588561f6daefe2415
Fernando Zanchetta <fernandozanca94@gmail.com>
2022-08-11 22:03:55 -0300

    feat: create greeting test

---
+++ src/components/Greeting.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ greeting }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Greeting',
+  data () {
+    return {
+      greeting: 'Vue and TDD'
+    }
+  }
+}
+</script>
