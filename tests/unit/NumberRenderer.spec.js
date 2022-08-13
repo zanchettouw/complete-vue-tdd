1956aa8aaf07c899f46bc315ecd0b597f88bf68e
Fernando Zanchetta <fernandozanca94@gmail.com>
2022-08-13 14:43:59 -0300

    testing computed

---
+++ tests/unit/NumberRenderer.spec.js
@@ -0,0 +1,18 @@
+import { mount } from '@vue/test-utils'
+import NumberRenderer from '@/components/NumberRenderer.vue'
+
+describe('NumberRenderer.vue', () => {
+  it('renders even numbers', () => {
+    const wrapper = mount(NumberRenderer, {
+      propsData: {
+        even: true
+      }
+    })
+    expect(wrapper.text()).toBe('2, 4, 6, 8')
+  })
+
+  it('renders odd numbers', () => {
+    const localThis = { even: false }
+    expect(NumberRenderer.computed.numbers.call(localThis)).toBe('1, 3, 5, 7, 9')
+  })
+})
