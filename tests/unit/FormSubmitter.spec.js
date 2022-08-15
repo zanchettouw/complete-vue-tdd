09abcc205401ba530d35cd0eab0e2d98660edb01
Fernando Zanchetta <fernandozanca94@gmail.com>
2022-08-15 19:37:11 -0300

    declare explicit wrapper.vm.

--- tests/unit/FormSubmitter.spec.js
+++ tests/unit/FormSubmitter.spec.js
@@ -1,12 +1,28 @@
 import { mount } from '@vue/test-utils'
+import flushPromises from 'flush-promises'
 import FormSubmitter from '@/components/FormSubmitter.vue'
+
+let url = ''
+let data = ''
+
+const mockHttp = {
+  get: (_url, _data) => {
+    return new Promise((resolve) => {
+      url = _url
+      data = _data
+      resolve()
+    })
+  }
+}
+
 
 describe('FormSubmitter', () => {
   it('reveals a notification when submitted', async () => {
     const wrapper = mount(FormSubmitter)
 
-    await wrapper.find('[data-username]').setValue('alice')
-    await wrapper.find('form').trigger('submit.prevent')
+    wrapper.find('[data-username]').setValue('alice')
+    wrapper.find('form').trigger('submit.prevent')
+    await wrapper.vm.$nextTick()
 
 
     expect(wrapper.find('.message').text())
