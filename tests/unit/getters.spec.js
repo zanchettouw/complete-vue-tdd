68d98690c6af94b515eec7e87b27cf3d96c63759
Fernando Zanchetta <fernandozanca94@gmail.com>
2022-09-14 21:41:34 -0300

    testing getters

---
+++ tests/unit/getters.spec.js
@@ -0,0 +1,26 @@
+import getters from '../../src/store/getters'
+
+const dogs = [
+  { name: 'lucky', breed: 'poodle', age: 1 },
+  { name: 'pochy', breed: 'dalmatian', age: 2 },
+  { name: 'blackie', breed: 'poodle', age: 4 }
+]
+
+const state = { dogs }
+
+describe('poodles', () => {
+  it('returns poodles', () => {
+    const actual = getters.poodles(state)
+
+    expect(actual).toEqual([dogs[0], dogs[2] ])
+  })
+})
+
+describe('poodlesByAge', () => {
+  it('return poodles by age', () => {
+    const poodles = [ dogs[0], dogs[2] ]
+    const actual = getters.poodlesByAge(state, { poodles })(1)
+
+    expect(actual).toEqual([ dogs[0] ])
+  })
+})
